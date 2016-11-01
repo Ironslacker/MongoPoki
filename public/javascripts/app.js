257bf85e7129c886cc4c346b3f85f392c846f712
Ironslacker <william.keele103@gmail.com>
2016-11-01 16:17:42 -0600

    finished...

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,6 +7,13 @@ function pokemonFetcher ($http) {
 
   var API_ROOT = 'pokemon'
   return {
+    post: function (formData) {
+      return $http
+         .post(API_ROOT,formData)
+         .then(function (resp) {
+           console.log("Post worked");
+         })
+    },
     get: function () {
       return $http
         .get(API_ROOT)
@@ -19,11 +26,18 @@ function pokemonFetcher ($http) {
 
 function mainCtrl ($scope, pokemonFetcher) {
 
-  $scope.pokemon = []
+  $scope.pokemon = [];
+
+  $scope.addPoki = function() {
+      var formData = {name:$scope.Name,avatarUrl:$scope.Url};
+      console.log(formData);
+      pokemonFetcher.post(formData); // Send the data to the back end
+      $scope.pokemon.push(formData); // Update the model
+    }
 
   pokemonFetcher.get()
     .then(function (data) {
       $scope.pokemon = data
-    })
+    });
 
 }
